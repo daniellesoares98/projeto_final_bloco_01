70d2b36fc7dd24a910ca19104bb6700780c6e6c6
danielle soares <contato.daniellesoares@gmail.com>
2026-06-30 10:44:33 -0300

    commite inicial

---
+++ src/projeto_final_bloco_01/Menu.java
@@ -0,0 +1,19 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	private static final Scanner leia = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int opcao;
+		
+		System.out.println("**********************************************");
+		System.out.println("                                              ");
+		System.out.println("               PET SHOT ONLINE                ");
+		System.out.println("                                              ");
+		System.out.println("**********************************************");
+	}
+}
